bd265d541069cdef24fa8ba2d0611736e6faa1db
muhammadmajedi89-max <muhammad.majedi89@gmail.com>
2026-06-10 01:29:10 +0400

    feat: add Sanity schema registry

---
+++ sanity/schema.ts
@@ -0,0 +1,7 @@
+import { career } from './schemas/career';
+import { portfolio } from './schemas/portfolio';
+import { post } from './schemas/post';
+import { seo } from './schemas/seo';
+import { service } from './schemas/service';
+
+export const schemaTypes = [post, portfolio, career, service, seo];
