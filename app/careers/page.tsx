d3a36ad21d367a995d31fd54c7e2d206fae8731a
muhammadmajedi89-max <muhammad.majedi89@gmail.com>
2026-06-10 08:38:58 +0400

    feat: connect careers page to Sanity

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -5,15 +5,18 @@ import { HiringProcess } from '@/components/careers/hiring-process';
 import { OpenRoles } from '@/components/careers/open-roles';
 import { Footer } from '@/components/layout/footer';
 import { Header } from '@/components/layout/header';
+import { getCareers } from '@/lib/sanity/fetchers';
 
-export default function CareersPage() {
+export default async function CareersPage() {
+  const careers = await getCareers();
+
   return (
     <main className="min-h-screen overflow-hidden bg-brand-bg text-brand-ink">
       <div className="absolute left-0 right-0 top-0 z-50 pt-4">
         <Header />
       </div>
       <CareersHero />
-      <OpenRoles />
+      <OpenRoles cmsRoles={careers} />
       <Benefits />
       <HiringProcess />
       <CareersCTA />
